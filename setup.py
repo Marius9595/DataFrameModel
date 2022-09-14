65fd7824c1826c11a2c602cb5c5e04729b793e53
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-09-15 00:33:52 +0100

    deployable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DataFrameModel',
+    version='0.1.0',
+    packages=['test', 'Errors'],
+    url='https://github.com/Marius9595/DataFrameModel.git',
+    license='MIT',
+    author='mario',
+    author_email='mariospmdev@gmail.com',
+    description='A wrapper of DataFrames to encapsulate and get explicity for your dataframes'
+)
